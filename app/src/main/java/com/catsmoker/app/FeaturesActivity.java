17e61c96424c23ef5ab72bc79bbaf06bdeccefe1
catsmoker <boulhada08@gmail.com>
2026-01-29 16:13:10 +0100

    fix

--- app/src/main/java/com/catsmoker/app/FeaturesActivity.java
+++ app/src/main/java/com/catsmoker/app/FeaturesActivity.java
@@ -57,7 +57,6 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
